65076734551a682430c3b481520435eb412aec1f
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2023-04-06 22:47:49 -0300

    Add .py files

---
+++ pyFiles/Factorial.py
@@ -0,0 +1,28 @@
+class Factorial:
+
+    def __init__(self, want_show=False):
+        self.want_show = want_show
+
+    def simple(self, num):
+        result = 1
+        for number in range(num, 0, -1):
+            if self.want_show:
+                if number != 1:
+                    print(f"{number} x", end=" ")
+                else:
+                    print(f"{number}")
+
+            result *= number
+
+        return result
+
+    def repetition(self, n, r):
+        numerator = self.simple(n)
+        denominator = 1
+        for number in r:
+            denominator *= self.simple(number)
+
+        return numerator/denominator
+
+    def circular(self, n):
+        return self.simple(n-1)
---
+++ pyFiles/Test.py
@@ -0,0 +1,9 @@
+import Factorial
+
+fat = Factorial.Factorial(want_show=True)
+
+print(fat.simple(5))
+print("--------------------------------------------")
+print(fat.repetition(7, (3, 2, 2)))
+print("--------------------------------------------")
+print(fat.circular(5))
